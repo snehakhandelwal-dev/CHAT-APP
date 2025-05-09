61748a05bbce763c96a8c29c25bd31e4231064d6
snehakhandelwal-dev <sneha.developer29@gmail.com>
2025-05-09 14:25:32 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // Ensures you're using modern JS features
+    outDir: 'dist', // The output directory for your build (default is dist)
+  },
+  server: {
+    port: 3000, // Custom dev server port
+    open: true, // Automatically opens the browser when the server starts
+  },
 })
